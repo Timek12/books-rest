3a4bc17e851a2c863aee65b068144cd37274222f
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-27 22:32:57 +0100

    Implement book service with create method

---
+++ src/main/java/com/tim/books/services/implementations/BookService.java
@@ -0,0 +1,38 @@
+package com.tim.books.services.implementations;
+
+import com.tim.books.domain.Book;
+import com.tim.books.domain.BookEntity;
+import com.tim.books.repositories.BookRepository;
+import com.tim.books.services.interfaces.IBookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService implements IBookService {
+
+    private final BookRepository bookRepository;
+    public BookService(final BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    @Override
+    public Book create(final Book book) {
+        final BookEntity bookEntity = bookToBookEntity(book);
+        final BookEntity savedBookEntity = bookRepository.save(bookEntity);
+        return bookEntityToBook(savedBookEntity);
+    }
+
+    private BookEntity bookToBookEntity(Book book) {
+        return BookEntity.builder()
+                .author(book.getAuthor())
+                .isbn(book.getIsbn())
+                .title(book.getTitle())
+                .build();
+    }
+
+    private Book bookEntityToBook(BookEntity bookEntity) {
+        return Book.builder()
+                .author(bookEntity.getAuthor())
+                .isbn(bookEntity.getIsbn())
+                .title(bookEntity.getTitle())
+                .build();
+    }
+}
