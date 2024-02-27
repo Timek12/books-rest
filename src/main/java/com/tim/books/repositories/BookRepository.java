3e3a2e0bee8b9e9657610fa31e8e363a7f756a42
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-27 22:32:37 +0100

    Create book repository

---
+++ src/main/java/com/tim/books/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package com.tim.books.repositories;
+
+import com.tim.books.domain.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, String> {
+
+}
