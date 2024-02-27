d5045197cb019b18b050410ad9ee861cbb9d023b
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-02-28 00:16:57 +0100

    Add listBooks method to interface

--- src/main/java/com/tim/books/services/interfaces/IBookService.java
+++ src/main/java/com/tim/books/services/interfaces/IBookService.java
@@ -2,9 +2,11 @@ package com.tim.books.services.interfaces;
 
 import com.tim.books.domain.Book;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IBookService {
     Book create(Book book);
     Optional<Book> findById(String isbn);
+    List<Book> listBooks();
 }
